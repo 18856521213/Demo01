3925d45af78b024a64314ec2a869090f97f8ab78
e <1332637611@qq.com>
2019-09-20 15:52:03 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <div id="app">
+      <div class="box">
+            <div class="small-box">
+              我这是小盒子，最后丢了真爱，丢了美好幸福
+            </div>
+            <div class="big-box">
+              我这是大盒子
+            </div>
+
+      </div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style lang="less">
+#app {
+  width:300px;
+  height:150px;
+  background-color: azure;
+  .box {
+    .small-box {
+      font-size: 20px;
+      color: pink;
+    }
+
+    .big-box {
+      font-size: 14px;
+      color: black;
+    }
+  }
+  }
+</style>
